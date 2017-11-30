ee2df67fb51cf23cec2200ddd5c4a2a9a9839abf
daaaah46 <dahyek46@gmail.com>
2017-11-30 12:26:13 +0900

    commit 171130

--- app/src/main/java/com/hansung/congcheck/Activity/UserRegisterActivity.java
+++ app/src/main/java/com/hansung/congcheck/Activity/UserRegisterActivity.java
@@ -1,30 +1,114 @@
 package com.hansung.congcheck.Activity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.hansung.congcheck.POJO.UserInfoList;
 import com.hansung.congcheck.R;
+import com.hansung.congcheck.Retrofit2.HttpClient;
+import com.hansung.congcheck.Retrofit2.HttpService;
+import com.hansung.congcheck.Utility.SharedPrefManager;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserRegisterActivity extends AppCompatActivity {
 
+    String TAG = "UserRegisterActivity";
+
     EditText phoneNumber;
+    Button registerConfirm;
+    Button.OnClickListener onClickListener;
+
+    List<UserInfoList.UserInfo> userInfo;
+    SharedPrefManager pref;
+    String userNumber = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_register);
-        phoneNumber = (EditText)findViewById(R.id.et_register_phoneNumber);
+        pref = SharedPrefManager.getInstance(this);
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 
-        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        String userNumber = "";
-        try{
+        try {
             userNumber = telephonyManager.getLine1Number();
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
-        phoneNumber.setText("0"+userNumber);
+        Log.e(TAG, userNumber);
+
+        if(pref.getPrefDataUsernumber() != null){
+
+            Intent intent = new Intent(UserRegisterActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }else{
+            //Retrofit2 동기방식
+            new AsyncTask<Void, Void, String>(){
+                @Override
+                protected String doInBackground(Void... voids) {
+                    HttpService api = HttpClient.getStationListService();
+                    Call<UserInfoList> call = api.getUserInfo(userNumber);
+
+                    try{
+                        Log.e(TAG, "불림..");
+                        userInfo = call.execute().body().getUserInfo();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    if(userInfo.size() == 0){
+                        setContentView(R.layout.activity_user_register);
+                        phoneNumber = (EditText) findViewById(R.id.et_register_userphoneNum);
+                        registerConfirm = (Button) findViewById(R.id.btn_register_confirm);
+                        Toast.makeText(getApplicationContext(), "기존에 없는 사용자 입니다.", Toast.LENGTH_SHORT).show();
+                        phoneNumber.setText(userNumber);
+                        OnClickListenerSetting();
+                        registerConfirm.setOnClickListener(onClickListener);
+                    }else{
+                        Toast.makeText(getApplicationContext(), "기존에 있는 사용자 입니다.", Toast.LENGTH_SHORT).show();
+                        UserPrefRegister();
+                    }
+                }
+            }.execute();
+
+        }
+    }
+
+    private void UserPrefRegister(){
+        pref.setPrefDataUsernumber(userNumber);
+        Intent intent = new Intent(UserRegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void OnClickListenerSetting(){
+        onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Register User Success!", Toast.LENGTH_SHORT).show();
+                UserPrefRegister();
+            }
+        };
     }
 }
